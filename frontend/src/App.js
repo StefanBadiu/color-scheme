564963433225f8c68622a9b7be4cfdcc7c12cc06
Stefan Badiu <sbadiu@purdue.edu>
2025-05-18 23:17:22 -0400

    experimenting with css styling (incl. header, button)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
@@ -11,27 +10,22 @@ function App() {
       .then(data => setMessage(data.message));
   }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+  return (<>
+    <div className="">
+      <header className="header">
+        <p className="">
+          Stefan Color Picker
         </p>
-        <p>
-          Message: {message || "Loading..."}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+    
+    <div className="main mt-[15px] flex flex-col gap-4">
+      <p>
+        API sample message: {message || "Loading..."} 
+      </p>
+      <button class="upload-button" type="button">Upload image</button>
+    </div>
+  </>);
 }
 
 export default App;
