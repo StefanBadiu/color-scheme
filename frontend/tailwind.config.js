3da544a29d79eb38f12dd0f37568ab87c1c7e0e2
Stefan Badiu <sbadiu@purdue.edu>
2025-05-18 23:05:05 -0400

    package fixes

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all React files
+    "./public/index.html",        // Include the HTML entry point
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
