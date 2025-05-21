e50b98411aedb955ab938d2835f6d5a6061dd7e6
Stefan Badiu <sbadiu@purdue.edu>
2025-05-21 13:04:32 -0400

    backend code for splitting image colors

--- backend/venv/main.py
+++ backend/venv/main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI, File, UploadFile, Form, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 import numpy as np
+import logging
     
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+logging.basicConfig(level=logging.INFO)
     
 @app.get("/api/test")
 async def hello():
@@ -20,10 +21,30 @@ async def hello():
 @app.post("/api/colorscheme")
 async def colorscheme(file: UploadFile = File(...), colorCount: int = Form(...)):
     try:
+        logging.info("Received file: %s", file.filename)
+        logging.info("Requested color count: %d", colorCount)
+        
         img = Image.open(file.file)
-        # Sample
-        colors = ["#FF5733", "#33FF57", "#3357FF"][:colorCount]
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+        logging.info("Image opened successfully.")
 
-    return {"message": f"COLOR SCHEME (API version) !! colors in image = {colors}"}
+        image_array = np.array(img)
+        logging.info("Image converted to array: %s", image_array.shape)
+        logging.info("Dimensions in image: %s", image_array.shape[2])
+
+        colors = {}
+
+        for x in range(image_array.shape[0]):
+            for y in range(image_array.shape[1]):
+                # logging.info("image_array[x][y]: %s", image_array[x][y])
+                if str(image_array[x][y]) not in colors:
+                    colors[str(image_array[x][y])] = 1
+                    # logging.info("one")
+                else:
+                    colors[str(image_array[x][y])] += 1
+                    # logging.info("two")
+
+        logging.info("colors: %s", colors)
+    except Exception as e:
+        logging.info(f"Error: {e}")
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+    return {"message": colors}
